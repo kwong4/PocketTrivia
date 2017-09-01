cfc216d943a08f5a0f23e21eb0d5943542a0de6c
kwong4 <kwong4@ualberta.ca>
2017-09-01 00:13:03 -0600

    Initial pages & functionality

--- PocketTrivia.c
+++ PocketTrivia.c
@@ -8,7 +8,7 @@ using namespace std;
 // Change Questions answers to char* and malloc correct amount of space.
 // Do this in conjuction with a cleanup function (free)
 
-void parse_options(Option *options, const char* textfile) {
+void parse_options(struct Option options[], const char* textfile) {
 	
 	FILE *file = fopen(textfile, "r");
 	char line[256];
@@ -22,17 +22,7 @@ void parse_options(Option *options, const char* textfile) {
 			if (strncmp(line, "-", 1) == 0) {
 				current_option++;
 				strncpy(options[current_option].str_name, &line[1], end_position);
-				strncpy(options[current_option].str_name, "\0", 1);
-				fgets(line, sizeof(line), file);
-				end_position = strcspn(line, "\n") - 1;
-				strncpy(line, "\0", 1);
-				sscanf(line, "%d", &current_file);
-				char **temp = (char **) malloc(current_file * sizeof(char *));
-				for (i = 0; i < current_file; i++) {
-					temp[i] = (char *) malloc (MAX_WORD * sizeof(char));
-				}
-				options[current_option].str_filename = temp;
-				current_file = 0;
+				strncpy(&options[current_option].str_name[end_position], "\0", 1);
 			}
 			else {
 				strncpy(options[current_option].str_filename[current_file], &line[1], end_position);
@@ -47,28 +37,29 @@ void parse_options(Option *options, const char* textfile) {
 	}
 }
 
-void parse_questions(Question *questions, const char* textfile) 
+void parse_questions(Question questions[], const char* textfile) 
 {
 	FILE *file = fopen(textfile, "r");
 	char line[MAX_WORD];
 	
 	int answer = 0;
 	int end_position = 0;
+	int current_question = -1;
 	
 	if (file != NULL) {
 		while(fgets(line, sizeof(line), file)) {
 			end_position = strcspn(line, "\n") - 1;
-			
 			if (strncmp(line, "-", 1) == 0) {
-				strncpy(questions->str_question, &line[1], end_position);
-				strncpy(&questions->str_question[end_position], "\0", 1);
+				current_question++;
+				strncpy(questions[current_question].str_question, &line[1], end_position);
+				strncpy(&questions[current_question].str_question[end_position], "\0", 1);
 			}
 			else {
 				if (strncmp(line, "*", 1) == 0) {
-					questions->answer = answer;
+					questions[current_question].answer = answer;
 				}
-				strncpy(questions->str_answer[answer], &line[1], end_position);
-				strncpy(&questions->str_answer[answer][end_position], "\0", 1);
+				strncpy(questions[current_question].str_answer[answer], &line[1], end_position);
+				strncpy(&questions[current_question].str_answer[answer][end_position], "\0", 1);
 				answer++;
 			}
 		}
@@ -82,36 +73,81 @@ void parse_questions(Question *questions, const char* textfile)
 
 int main(void)
 {
-    //initialize Allegro
+	
+    //Initialize Allegro
 	allegro_init();
     install_keyboard();
     install_timer();
     srand(time(NULL));
+    set_color_depth(desktop_color_depth());
     
+    //Initalize variables
+    int i;
+    int position;
+    
+    //Initialize Graphics
     int ret;
-    
-    printf("test\n");
-    
     ret = set_gfx_mode(MODE, WIDTH, HEIGHT, 0, 0);
     if (ret != 0) {
         allegro_message(allegro_error);
         return 0;
     }
+    /*
+    //Print initial Start Screen with instructions
+    textout_centre_ex(screen, font, "Pocket Trivia", WIDTH/2, HEIGHT/4, WHITE, BLACK);
     
+    textout_centre_ex(screen, font, "Press ENTER to Start!", WIDTH/2, HEIGHT/2, YELLOW, BLACK);
+    
+    textout_centre_ex(screen, font, "INSTRUCTIONS:", WIDTH/2, HEIGHT/2 + 40, WHITE, BLACK);
+    textout_ex(screen, font, "1. Use the arrow keys to navigate menu selection", WIDTH/8, HEIGHT/2 + 60, WHITE, BLACK);
+    textout_ex(screen, font, "2. Use the ENTER key to select an option", WIDTH/8, HEIGHT/2 + 70, WHITE, BLACK);
+    textout_ex(screen, font, "3. Then use the NUMBER keys to choose an answer for a question", WIDTH/8, HEIGHT/2 + 80, WHITE, BLACK);
+    textout_ex(screen, font, "4. Press Esc to exit the game!", WIDTH/8, HEIGHT/2 + 90, WHITE, BLACK);
+    
+    //Wait until user press Enter
+    while(!key[KEY_ENTER]) {
+		rest(100);
+	};
+	// Clear screen
+    rectfill(screen, 0, 0, WIDTH, HEIGHT, BLACK);
+    
+    */
+    struct Option topics[TOPICNUMBER];
+    //topics = (struct Option*) malloc(TOPICNUMBER * sizeof(struct Option));
+    
+    parse_options(topics, TOPIC);
+    
+    position = 60;
+    textout_ex(screen, font, "Please select a topic!", WIDTH/8, HEIGHT/2 + 50, WHITE, BLACK);
+    
+    for(i = 0; i < TOPICNUMBER; i++) {
+    	position += 10;
+    	textprintf_ex(screen, font, WIDTH/8, HEIGHT/2 + position, WHITE, BLACK, "   %s", topics[i].str_name);
+    }
+    
+    
+    //textout_ex(screen, font, "2. Use the ENTER key to select an option", WIDTH/8, HEIGHT/2 + 80, WHITE, BLACK);
+    //textout_ex(screen, font, "3. Then use the NUMBER keys to choose an answer for a question", WIDTH/8, HEIGHT/2 + 90, WHITE, BLACK);
+    
+    //Wait until user press Enter
+    while(!key[KEY_ESC]) {
+		rest(100);
+	};
+    
+    /*
     Question* question1;
     question1 = (struct Question*) malloc(1 * sizeof(struct Question));
     parse_questions(question1, "Questions\\chapter1x.txt");
     
-    textprintf_ex(screen, font, 0, 30, 15, -1, "%s", question1->str_question);
-    textprintf_ex(screen, font, 0, 40, 15, -1, "A. %s", question1->str_answer[0]);
-    textprintf_ex(screen, font, 0, 50, 15, -1, "B. %s", question1->str_answer[1]);
-    textprintf_ex(screen, font, 0, 60, 15, -1, "C. %s", question1->str_answer[2]);
-    textprintf_ex(screen, font, 0, 70, 15, -1, "D. %s", question1->str_answer[3]);
+    textprintf_ex(screen, font, 0, 30, WHITE, BLACK, "%s", question1->str_question);
+    textprintf_ex(screen, font, 0, 40, WHITE, BLACK, "A. %s", question1->str_answer[0]);
+    textprintf_ex(screen, font, 0, 50, WHITE, BLACK, "B. %s", question1->str_answer[1]);
+    textprintf_ex(screen, font, 0, 60, WHITE, BLACK, "C. %s", question1->str_answer[2]);
+    textprintf_ex(screen, font, 0, 70, WHITE, BLACK, "D. %s", question1->str_answer[3]);
+    */
     
-    while(!keypressed()) {
-	};
     
-    free(question1);
+    //free(question1);
     allegro_exit();
 	return 0;
 }     
--- PocketTrivia.h
+++ PocketTrivia.h
@@ -12,13 +12,19 @@ using namespace std;
 #define MODE GFX_AUTODETECT_WINDOWED
 #define WIDTH 640
 #define HEIGHT 480
+#define WHITE makecol(255,255,255)
+#define YELLOW makecol(221,216,0)
+#define BLACK makecol(0,0,0)
 #define MAX_WORD 256
 #define MAX_ANSWER 4
-#define TEXT texts.txt
+#define MAX_FILE 10
+#define TOPIC "topics.txt"
+#define TOPICNUMBER 1
+#define TEXT "texts.txt"
 #define TEXTNUMBER 1
-#define UNIT units.txt
+#define UNIT "units.txt"
 #define UNITNUMBER 4
-#define CHAPTER chapters.txt
+#define CHAPTER "chapters.txt"
 #define CHAPTERNUMBER 22
 
 //define question structure
@@ -32,7 +38,7 @@ struct Question
 struct Option
 {
 	char str_name[MAX_WORD];
-	char** str_filename;	
+	char str_filename[MAX_FILE][MAX_WORD];	
 };
 
 //functions
