88c6e8b2fe1433779af347a70bcb4db956d3c5c2
kwong4 <kwong4@ualberta.ca>
2017-09-05 23:47:55 -0600

    Cleanup and comments. Need to adjust selection text (To do later)

--- PocketTrivia.c
+++ PocketTrivia.c
@@ -15,14 +15,14 @@ int getinput() {
 	}
 	
 	if (key[KEY_DOWN] && selection != max_selection) {
-		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + SELECTION_BUFFER + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + SELECTION_BUFFER + selection * 15 + 7, BLACK);
+		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + selection * 15 + 7, BLACK);
 		selection++;
-		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + SELECTION_BUFFER + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + SELECTION_BUFFER + selection * 15 + 7, WHITE);
+		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 +  selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + selection * 15 + 7, WHITE);
 	}
 	else if (key[KEY_UP] && selection != 0) {
-		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + SELECTION_BUFFER + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + SELECTION_BUFFER + selection * 15 + 7, BLACK);
+		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 +  selection * 15 + 7, BLACK);
 		selection--;
-		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + SELECTION_BUFFER + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + SELECTION_BUFFER + selection * 15 + 7, WHITE);
+		rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + selection * 15 - 1, WIDTH/8 + 9, HEIGHT/4 + selection * 15 + 7, WHITE);
 	}
 	else if (key[KEY_ENTER]) {
 		return 1;
@@ -105,6 +105,73 @@ void parse_questions(Question questions[], const char* textfile)
 	}
 }
 
+void draw_startscreen() {
+	textout_centre_ex(screen, font, "Pocket Trivia", WIDTH/2, HEIGHT/4, WHITE, BLACK);
+    textout_centre_ex(screen, font, "Press ENTER to Start!", WIDTH/2, HEIGHT/2, YELLOW, BLACK);
+    textout_centre_ex(screen, font, "INSTRUCTIONS:", WIDTH/2, HEIGHT/2 + 40, WHITE, BLACK);
+    textout_ex(screen, font, "1. Use the arrow keys to navigate menu selection", WIDTH/8, HEIGHT/2 + 60, WHITE, BLACK);
+    textout_ex(screen, font, "2. Use the ENTER key to select an option", WIDTH/8, HEIGHT/2 + 70, WHITE, BLACK);
+    textout_ex(screen, font, "3. Then use the NUMBER keys to choose an answer for a question", WIDTH/8, HEIGHT/2 + 80, WHITE, BLACK);
+    textout_ex(screen, font, "4. Press Esc to exit the game!", WIDTH/8, HEIGHT/2 + 90, WHITE, BLACK);
+}
+
+void print_options(Option options[], const char* title, int max, int cont) {
+	int i;
+	int position = 0;
+	
+	// Print title
+	textprintf_ex(screen, font, WIDTH/8, HEIGHT/4 - 20, WHITE, BLACK, "%s", title);
+	
+	for(i = 0; i < max; i++) {
+    	rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
+    	textprintf_ex(screen, font, WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK, "%s", options[i].str_name);
+    	position += 15;
+    }
+    
+    if (cont == 1) {
+    	rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
+	    textout_ex(screen, font, "Continue", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
+    }
+    
+    selection = 0;
+    rectfill(screen, WIDTH/8 + 1, HEIGHT/4 - 1, WIDTH/8 + 9, HEIGHT/4 + 7, WHITE);
+}
+
+void print_textbook_options() {
+	int position = 0;
+    textout_ex(screen, font, "Please select an option to choose your questions!", WIDTH/8, HEIGHT/4 - 20, WHITE, BLACK);
+    
+    rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
+    textout_ex(screen, font, "All Questions (Whole textbook)", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
+    position += 15;
+    rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
+    textout_ex(screen, font, "Choose by Unit", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
+    position += 15;
+    rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
+    textout_ex(screen, font, "Choose by Chapter", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
+    
+    rectfill(screen, WIDTH/8 + 1, HEIGHT/4 - 1, WIDTH/8 + 9, HEIGHT/4 + 7, WHITE);
+}
+
+void parse_all_questions(Question questions[], const char* folderpath) {
+	FILE *file = fopen(concat(folder_topic, TEXT), "r");
+	char line[256];
+	int end_position;
+	int current_file = 0;
+	
+	if (file != NULL) {
+    	while(fgets(line, sizeof(line), file)) {
+    		end_position = strcspn(line, "\n");
+    		strncpy(&line[end_position], "\0", 1);
+    		parse_questions(&questions[current_file * MAX_QUESTIONS_PER_FILE], concat(folderpath, line));
+    	}
+	}
+	else {
+		allegro_exit();
+	}
+	fclose(file);
+}
+
 int main(void)
 {
 	
@@ -130,15 +197,7 @@ int main(void)
     }
     
     //Print initial Start Screen with instructions
-    textout_centre_ex(screen, font, "Pocket Trivia", WIDTH/2, HEIGHT/4, WHITE, BLACK);
-    
-    textout_centre_ex(screen, font, "Press ENTER to Start!", WIDTH/2, HEIGHT/2, YELLOW, BLACK);
-    
-    textout_centre_ex(screen, font, "INSTRUCTIONS:", WIDTH/2, HEIGHT/2 + 40, WHITE, BLACK);
-    textout_ex(screen, font, "1. Use the arrow keys to navigate menu selection", WIDTH/8, HEIGHT/2 + 60, WHITE, BLACK);
-    textout_ex(screen, font, "2. Use the ENTER key to select an option", WIDTH/8, HEIGHT/2 + 70, WHITE, BLACK);
-    textout_ex(screen, font, "3. Then use the NUMBER keys to choose an answer for a question", WIDTH/8, HEIGHT/2 + 80, WHITE, BLACK);
-    textout_ex(screen, font, "4. Press Esc to exit the game!", WIDTH/8, HEIGHT/2 + 90, WHITE, BLACK);
+    draw_startscreen();
     
     //Wait until user press Enter
     while(1) {
@@ -149,31 +208,22 @@ int main(void)
     		allegro_exit();
     		exit(0);
     	}
-		rest(100);
 	};
 	
 	// Clear screen
     rectfill(screen, 0, 0, WIDTH, HEIGHT, BLACK);
     
+    // Obtain topic options for the user
     struct Option topics[TOPICNUMBER];
-    //topics = (struct Option*) malloc(TOPICNUMBER * sizeof(struct Option));
-    
     parse_options(topics, TOPIC);
     
-    position = SELECTION_BUFFER;
-    textout_ex(screen, font, "Please select a topic!", WIDTH/8, HEIGHT/4 + SELECTION_BUFFER - 20, WHITE, BLACK);
+    // Print options for user
+    print_options(topics, "Please select a topic!", TOPICNUMBER, 0);
     
-    for(i = 0; i < TOPICNUMBER; i++) {
-    	rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
-    	textprintf_ex(screen, font, WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK, "%s", topics[i].str_name);
-    	position += 15;
-    }
-    
-    rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + SELECTION_BUFFER - 1, WIDTH/8 + 9, HEIGHT/4 + SELECTION_BUFFER + 7, WHITE);
     max_selection = TOPICNUMBER - 1;
-    //textout_ex(screen, font, "2. Use the ENTER key to select an option", WIDTH/8, HEIGHT/2 + 80, WHITE, BLACK);
-    //textout_ex(screen, font, "3. Then use the NUMBER keys to choose an answer for a question", WIDTH/8, HEIGHT/2 + 90, WHITE, BLACK);
+
     rest(1000);
+    
     //Wait until user press Enter
     while(1) {
     	if (getinput() == 1) {
@@ -189,26 +239,11 @@ int main(void)
     
     strcpy(folder_topic, topics[selection].str_filename[0]);
     
-    position = SELECTION_BUFFER;
-    textout_ex(screen, font, "Please select an option to choose your questions!", WIDTH/8, HEIGHT/4 + SELECTION_BUFFER - 20, WHITE, BLACK);
-    
-    rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
-    textout_ex(screen, font, "All Questions (Whole textbook)", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
-    position += 15;
-    rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
-    textout_ex(screen, font, "Choose by Unit", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
-    position += 15;
-    rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
-    textout_ex(screen, font, "Choose by Chapter", WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK);
-    
-    rectfill(screen, WIDTH/8 + 1, HEIGHT/4 + SELECTION_BUFFER - 1, WIDTH/8 + 9, HEIGHT/4 + SELECTION_BUFFER + 7, WHITE);
+    // Print textbook options for user
+    print_textbook_options();
     
     max_selection = 2;
     selection = 0;
-    
-    //position += 15;
-    
-    //textprintf_ex(screen, font, WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK, "%s", concat(folder_topic, "test"));
     
     rest(1000);
     
@@ -218,90 +253,40 @@ int main(void)
     	}
 		rest(100);
 	};
+	
     // Clear screen
     rectfill(screen, 0, 0, WIDTH, HEIGHT, BLACK);
     
-    current_file = 0;
     if (selection == 0) {
     	questions = (Question *) malloc(sizeof(Question) * MAX_CHAPTERNUMBER);
-    	FILE *file = fopen(concat(folder_topic, TEXT), "r");
-    	char line[256];
     	
-    	if (file != NULL) {
-	    	while(fgets(line, sizeof(line), file)) {
-	    		end_position = strcspn(line, "\n");
-	    		strncpy(&line[end_position], "\0", 1);
-	    		textprintf_ex(screen, font, WIDTH/8 + 24, HEIGHT/4 + position + 40, WHITE, BLACK, "%s", concat(folder_topic, concat(QUESTION_FOLDER, line)));
-	    		while(!key[KEY_LEFT]) {
-	    			rest(100);	
-	    		}
-	    		parse_questions(&questions[current_file * MAX_QUESTIONS_PER_FILE], concat(folder_topic, concat(QUESTION_FOLDER, line)));
-	    	}
-    	}
-    	else {
-    		while(!key[KEY_LEFT]) {
-    			rest(100);	
-    		}
-    		allegro_exit();
-		    return 0;
-    	}
-    	fclose(file);
+    	parse_all_questions(questions, concat(folder_topic, QUESTION_FOLDER));
+    	
     	/*
-	    	for (i = 0; i < MAX_CHAPTERNUMBER; i++) {
-	    		current_file = 0;
-	    		FILE *file = fopen(concat(folder_topic, TEXT), "r");
-	    		if (file != NULL) {
-					char line[256];
-					while(fgets(line, sizeof(line), file)) {
-						
-					}
-					int current_option = -1;
-					int end_position = 0;
-					int current_file = 0;
-					int i;
-				}
-				else {
-		    		allegro_exit();
-		    		return 0;
-		    	}
-	    	}
-    	fclose(file);
-    	*/
-    	//parse_questions(questions, concat(folder_topic, TEXT));
-    	//parse_questions(questions, 
-    	//load all
-    	//use concat
     	textprintf_ex(screen, font, 0, 30, WHITE, BLACK, "%s", questions[0].str_question);
 	    textprintf_ex(screen, font, 0, 40, WHITE, BLACK, "A. %s", questions[0].str_answer[0]);
 	    textprintf_ex(screen, font, 0, 50, WHITE, BLACK, "B. %s", questions[0].str_answer[1]);
 	    textprintf_ex(screen, font, 0, 60, WHITE, BLACK, "C. %s", questions[0].str_answer[2]);
 	    textprintf_ex(screen, font, 0, 70, WHITE, BLACK, "D. %s", questions[0].str_answer[3]);
+	    */
     }
     else if (selection == 1) {
     	struct Option units[MAX_UNITNUMBER];
-    	//questions = (Question *) malloc(sizeof(Question) * MAX_UNITNUMBER);
-    	position = 0;
-    	textout_ex(screen, font, "Please select all units you wish to be tested on!", WIDTH/8, HEIGHT/4 - 20, WHITE, BLACK);
     	parse_options(units, concat(folder_topic, UNIT));
-    	for (i = 0; i < MAX_UNITNUMBER; i++) {
-    		rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
-	    	textprintf_ex(screen, font, WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK, "%s", units[i].str_name);
-	    	position += 15;
-    	}
+    	
+    	// Print options for user
+    	print_options(units, "Please select all units you wish to be tested on!", MAX_UNITNUMBER, 1);
+    	max_selection = MAX_UNITNUMBER;
     }
     else {
     	struct Option chapters[MAX_CHAPTERNUMBER];
-    	//questions = (Question *) malloc(sizeof(Question) * MAX_UNITNUMBER);
-    	position = 0;
-    	textout_ex(screen, font, "Please select all chapters you wish to be tested on!", WIDTH/8, HEIGHT/4 - 20, WHITE, BLACK);
     	parse_options(chapters, concat(folder_topic, CHAPTER));
-    	for (i = 0; i < MAX_CHAPTERNUMBER; i++) {
-    		rect(screen, WIDTH/8, HEIGHT/4 + position - 2, WIDTH/8 + 10, HEIGHT/4 + position + 8, WHITE);
-	    	textprintf_ex(screen, font, WIDTH/8 + 24, HEIGHT/4 + position, WHITE, BLACK, "%s", chapters[i].str_name);
-	    	position += 15;
-    	}
+    	
+    	// Print options for user
+    	print_options(chapters, "Please select all chapters you wish to be tested on!", MAX_CHAPTERNUMBER, 1);
+    	max_selection = MAX_CHAPTERNUMBER;
     }
-    
+	
     /*
     Question* question1;
     question1 = (struct Question*) malloc(1 * sizeof(struct Question));
@@ -314,11 +299,13 @@ int main(void)
     textprintf_ex(screen, font, 0, 70, WHITE, BLACK, "D. %s", question1->str_answer[3]);
     */
     
-    while(!key[KEY_ESC]) {
+    while(1) {
+		if (getinput() == 1 && selection == max_selection) {
+    		break;
+    	}
 		rest(100);
 	};
     free(questions);
-    //free(question1);
     allegro_exit();
 	return 0;
 }     
